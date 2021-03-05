e115b3e07d53e846297511b29c403f607f84223b
KomalBaviskar <komalshashikant@gmail.com>
2021-03-05 20:09:16 +0530

    hsdhdh

---
+++ CFA_Looker_automation/src/testScript/CallLogLookerReport.java
@@ -0,0 +1,36 @@
+package testScript;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import configuration.Configuration;
+import functionLibrary.ApplicationLibrary;
+
+public class CallLogLookerReport {
+WebDriver dr;
+	
+	@Test(priority=1)
+	public void test1()
+	{
+		ApplicationLibrary lib= new ApplicationLibrary();
+		
+		dr=lib.launchBrowser("Chrome");
+		dr.get(Configuration.url);
+		
+	}
+	
+	@Test(priority=2)
+	public void CFAlogin() throws InterruptedException
+	{
+		Thread.sleep(3000);
+		dr.findElement(By.id("email")).sendKeys("snehal.d@moentek.com");
+		
+		dr.findElement(By.id("password")).sendKeys("lmc2demo");
+		
+		dr.findElement(By.id("b1")).click();
+		
+		
+	}
+
+}
---
+++ CFA_Looker_automation/src/testing/TestLogic.java
@@ -0,0 +1,23 @@
+package testing;
+
+import org.openqa.selenium.WebDriver;
+
+import configuration.Configuration;
+import functionLibrary.ApplicationLibrary;
+
+public class TestLogic {
+
+public static void main(String[] args) {
+		
+		//D:\MoenTekRepo\CFAAutomation\BrowserDrivers\Chrome\89\chromedriver.exe
+	
+		
+		
+		System.out.println(System.getProperty("user.dir")+"\\BrowserDriver\\Chrome\\89\\chromedriver.exe");
+
+		ApplicationLibrary lib= new ApplicationLibrary();
+		
+		WebDriver dr=lib.launchBrowser("Chrome");
+		dr.get(Configuration.url);
+	}
+}
