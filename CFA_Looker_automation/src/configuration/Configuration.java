d6711712f718fe0c67897f5838dec21fbbe9a3f9
Rushabh <rushabhbhagyawant75@gmail.com>
2021-03-06 16:35:17 +0530

    2nd commit message

--- CFA_Looker_automation/src/configuration/Configuration.java
+++ CFA_Looker_automation/src/configuration/Configuration.java
@@ -5,6 +5,6 @@ public class Configuration {
 		public static String url="https://stag-5-cmo-1.convirza.com/#/login";
 		public static String chromeDriverPath=System.getProperty("user.dir")+"\\Browser\\Chrome\\89\\chromedriver.exe";
 
-	
+	//this is configuration file
 
 }
