bf8c274f64478fa4c7a4ddddc0f2fad21ac30be8
Rushabh <rushabhbhagyawant75@gmail.com>
2021-03-06 19:02:44 +0530

    Commited by Rushabh

--- CFA_Looker_automation/src/testScript/CallLogLookerReport.java
+++ CFA_Looker_automation/src/testScript/CallLogLookerReport.java
@@ -30,8 +30,34 @@ WebDriver dr;
 		
 		dr.findElement(By.id("b1")).click();
 		
-		
 	}
+	
+	@Test(priority=3)
+	public void NavigateToReportsPage() throws InterruptedException{
+		Thread.sleep(3000);
+		//click on reports icon
+		dr.findElement(By.xpath("//*[@id='sidebar']/li[2]/a/i")).click();
+		}
+	
+	@Test(priority=4)
+	public void SelectCallLogsReportFromSelectAReportList() throws InterruptedException{
+		//Select Call Logs Report From Select A Report List
+		Thread.sleep(3000);
+		dr.findElement(By.xpath("//span[@class='select2-arrow']")).click();
+		dr.findElement(By.xpath("//input[@id='s2id_autogen1_search']")).click();
+		dr.findElement(By.xpath("//input[@id='s2id_autogen1_search']")).sendKeys("call logs");
+		dr.findElement(By.xpath("//span[@class='select2-match']")).click();
+	}
+	
+	@Test(priority=5)
+	public void ClickOnDateRangeButton() throws InterruptedException{
+		//click on date range button
+		Thread.sleep(3000);
+		dr.findElement(By.xpath("//div[contains(@class,'Hover')]")).click();
+		////ul[@id='select2-results-1']
+	}
+	
+	
 	
 
 }
