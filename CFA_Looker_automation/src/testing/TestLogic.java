db8f38ca1b211f3624a9cec1e2d519a46ca6dcd0
KomalBaviskar <komalshashikant@gmail.com>
2021-03-06 16:45:20 +0530

    added comment in testLogic.java

--- CFA_Looker_automation/src/testing/TestLogic.java
+++ CFA_Looker_automation/src/testing/TestLogic.java
@@ -13,11 +13,12 @@ public static void main(String[] args) {
 	
 		
 		
-		System.out.println(System.getProperty("user.dir")+"\\BrowserDriver\\Chrome\\89\\chromedriver.exe");
+		System.out.println(System.getProperty("user.dir")+"\\Browser\\Chrome\\89\\chromedriver.exe");
 
 		ApplicationLibrary lib= new ApplicationLibrary();
 		
 		WebDriver dr=lib.launchBrowser("Chrome");
 		dr.get(Configuration.url);
+		//this is comment by komal..........
 	}
 }
